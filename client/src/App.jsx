ae129127fdd9ae5d98ed06237897810a75e81f7b
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2025-10-19 20:30:09 +0600

    add the sidebar chatcontainer rightsidebar in home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-[url("./src/assets/bgImage.svg")] bg-contain'>
+     <Routes>
+        <Route path='/' element = {<HomePage/>}/>
+        <Route path='/login' element = {<LoginPage/>}/>
+        <Route path='/profile' element = {<ProfilePage/>}/>
+      </Routes>
+    </div>
   )
 }
 
